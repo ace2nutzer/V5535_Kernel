57b1fad3db28960a4b11328e8a5d47e0e1fde6d5
ace2nutzer <ace2nutzer@gmail.com>
2024-08-08 20:03:06 +0200

    build_sisfb.sh: cleanup

--- build_sisfb.sh
+++ build_sisfb.sh
@@ -5,5 +5,3 @@ KERNEL=$(uname -r)
 
 make -j$JOBS drivers/video/fbdev/sis/ modules
 sudo cp drivers/video/fbdev/sis/sisfb.ko /lib/modules/$KERNEL/kernel/drivers/video/fbdev/sis
-#cd /etc/X11
-#sudo cp xorg.conf.fbdev xorg.conf
