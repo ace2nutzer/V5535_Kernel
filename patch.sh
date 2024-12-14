b5761db7940b00d2189232333ba6de7fe4639b31
ace2nutzer <ace2nutzer@gmail.com>
2024-12-14 01:51:55 +0100

    patch.sh: update

--- patch.sh
+++ patch.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ -f rejects ] ; then
+	rm rejects
+fi
+
 patch -p 1 -i $HOME/Downloads/patch-6.12.$@ -r rejects
 if [ -f rejects ] ; then
+	echo "#####################"
 	echo "###### REJECTS ######"
+	echo "#####################"
 fi
