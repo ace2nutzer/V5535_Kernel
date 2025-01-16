70df3c84c8e559cfaff09e40f61ff52cd56b23e6
ace2nutzer <ace2nutzer@gmail.com>
2025-01-16 03:26:19 +0100

    cpufreq: optimize ondemand_gov

--- drivers/cpufreq/cpufreq_ondemand.h
+++ drivers/cpufreq/cpufreq_ondemand.h
@@ -10,9 +10,6 @@
 
 struct od_policy_dbs_info {
 	struct policy_dbs_info policy_dbs;
-	unsigned int freq_lo;
-	unsigned int freq_lo_delay_us;
-	unsigned int freq_hi_delay_us;
 	unsigned int sample_type:1;
 };
 
@@ -20,7 +17,3 @@ static inline struct od_policy_dbs_info *to_dbs_info(struct policy_dbs_info *pol
 {
 	return container_of(policy_dbs, struct od_policy_dbs_info, policy_dbs);
 }
-
-struct od_dbs_tuners {
-	unsigned int powersave_bias;
-};
