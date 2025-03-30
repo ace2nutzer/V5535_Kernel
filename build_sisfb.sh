6d3eb159fe6df83bceeec01144cce6570e224343
ace2nutzer <ace2nutzer@gmail.com>
2025-03-31 01:33:37 +0200

    defconfig: update and remove 32-bit defconfig and scripts

--- build_sisfb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-N=$(nproc)
-KERNEL=$(uname -r)
-
-make -j $N drivers/video/fbdev/sis/ modules
-sudo cp drivers/video/fbdev/sis/sisfb.ko /lib/modules/$KERNEL/kernel/drivers/video/fbdev/sis
--- edit_kernel_32.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-N=$(nproc)
-
-make -j $N v5535_32_defconfig
-make -j $N menuconfig
-cp .config arch/x86/configs/v5535_32_defconfig
-make -j $N v5535_32_defconfig
-cp .config arch/x86/configs/v5535_32_defconfig
